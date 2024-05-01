d055ca8509c312e2171bc7b97ac2e1295231c7df
Jumin-01 <revin.veniamin2003@gmail.com>
2024-05-01 12:12:50 +0300

    Add project struct

---
+++ Lists/Catalog.cs
@@ -0,0 +1,45 @@
+﻿using Project_Revin_InternetShop.Enum;
+using Project_Revin_InternetShop.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Revin_InternetShop.Lists
+{
+    public class Catalog : Collections
+    {
+        
+        private Category category;
+        private List<Game> game = new List<Game>();
+        public Catalog()
+        {
+
+        }
+        public Catalog(Category cat)
+        {
+            Category = cat;
+        }
+        public Catalog(Category cat, List<Game> game)
+        {
+
+        }
+
+        public Category Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        public override void AddGame(Game game)
+        {
+            throw new NotImplementedException();
+        }
+        public override void RemoveGame(Game game)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
